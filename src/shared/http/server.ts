34e107295133710fc1ad65e3e5f8581364c47b4a
Vyctor <dev.vyctor@gmail.com>
2021-08-25 23:27:17 -0300

    configurando o dockerfile do nodejs e do postgres

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '../errors/AppError';
@@ -11,20 +11,18 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      });
-    }
-
-    return response.status(500).json({
+app.use((error: Error, request: Request, response: Response) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
       status: 'error',
-      message: 'Internal server error',
+      message: error.message,
     });
-  },
-);
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333);
