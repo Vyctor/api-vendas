008868c371f37763d648958e262828cff577a7ba
Vyctor <dev.vyctor@gmail.com>
2021-09-08 20:16:51 -0300

    servico para listar todos os usuarios

---
+++ src/modules/users/services/ListAllUsersService.ts
@@ -0,0 +1,14 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+class ListAllUsersService {
+  public async execute(): Promise<User[]> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    return usersRepository.find();
+  }
+}
+
+export default ListAllUsersService;
