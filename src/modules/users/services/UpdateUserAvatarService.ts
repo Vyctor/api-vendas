156d8251f7e546d2060aa0f9c8209f4ab82a31ec
Vyctor <dev.vyctor@gmail.com>
2021-09-08 22:22:33 -0300

    servico para atualizacao de avatar de usuario

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,40 @@
+import upload from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import path from 'path/posix';
+import fs from 'fs';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  userId: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ avatarFilename, userId }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findById(userId);
+
+    if (!user) {
+      throw new AppError('User does not exists!');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(upload.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
