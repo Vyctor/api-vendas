ea0871d16aa5f92037fdafaef6a0f87b737fce31
Vyctor <dev.vyctor@gmail.com>
2021-09-11 11:14:15 -0300

    controller de perfil de usuario

---
+++ src/modules/users/controllers/ProfileController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import ShowProfileService from '../services/ShowProfileService';
+import UpdateProfileService from '../services/UpdateProfileService';
+
+class ProfileController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+
+    const showProfileService = new ShowProfileService();
+
+    const user = await showProfileService.execute({ user_id });
+
+    return response.json(user);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+
+    const { name, email, password, old_password } = request.body;
+
+    const updateProfileService = new UpdateProfileService();
+
+    const user = await updateProfileService.execute({
+      user_id,
+      name,
+      email,
+      password,
+      old_password,
+    });
+
+    return response.json(user);
+  }
+}
+
+export default ProfileController;
