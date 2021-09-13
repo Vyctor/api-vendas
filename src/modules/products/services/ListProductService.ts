23e319280fee8f447f65d5ae0400be5b7be7af88
Vyctor <dev.vyctor@gmail.com>
2021-09-12 22:26:48 -0300

    implementando a classe redis cache

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,12 +1,17 @@
 import ProductsRepository from '@modules/products/typeorm/repositories/ProductsRepository';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
+import RedisCache from '@shared/cache/RedisCache';
 
 class ListProductService {
   async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductsRepository);
+    const redisCache = new RedisCache();
 
     const products = await productsRepository.find();
+
+    await redisCache.save('teste', 'teste');
+
     return products;
   }
 }
