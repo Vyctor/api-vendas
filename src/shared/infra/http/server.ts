27474c1cd915f54306a5b61a9c9bff320d6a8cd1
Vyctor <dev.vyctor@gmail.com>
2021-10-11 18:12:36 -0300

    refatorando server.ts pra informar porta em que o servidor subiu

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,5 +2,5 @@ import { app } from './app';
 
 app.listen(process.env.APP_API_PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('Server is running at => ', new Date());
+  console.log('Server is running at port: ', process.env.APP_API_PORT);
 });
